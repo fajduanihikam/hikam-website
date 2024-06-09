37a2116f0eec4b5730c1c32264563d889d027522
fajduanihikam <fajduanihikam@gmail.com>
2024-06-09 13:33:27 +0700

    mywebsite

--- api/urls.py
+++ api/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from mywebsite import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('example.urls')),
+    path('', include('mywebsite.urls')),
+    path('', views.home),
 ]
