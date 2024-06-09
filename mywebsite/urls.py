abc86f217b7b4da4e9d6f8011659e1b7163b9f9a
fajduanihikam <fajduanihikam@gmail.com>
2024-06-09 18:40:23 +0700

    mywebsite-static-resolve-4

--- mywebsite/urls.py
+++ mywebsite/urls.py
@@ -1,9 +1,9 @@
 # example/urls.py
 from django.urls import path
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from mywebsite.views import home
-
 
 urlpatterns = [
     path('', home),
 ]
+urlpatterns += staticfiles_urlpatterns()
